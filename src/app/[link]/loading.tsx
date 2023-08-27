a6995ac0cd8d61441f083a32034dc1a9a1b55c4f
vanxh <hello@vanxh.dev>
2023-08-28 01:47:36 +0530

    skeleton link loading

--- src/app/[link]/loading.tsx
+++ src/app/[link]/loading.tsx
@@ -1,7 +1,13 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Page() {
   return (
-    <div className="mx-auto h-full w-full max-w-3xl pb-16 pt-16">
-      <span>Loading...</span>
+    <div className="container mx-auto h-full w-full max-w-3xl pb-16 pt-16">
+      <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
+        {Array.from({ length: 24 }).map((_, i) => (
+          <Skeleton key={i} className="aspect-square h-full w-full" />
+        ))}
+      </div>
     </div>
   );
 }
