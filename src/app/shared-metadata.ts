fc84536c7d79ba0b46c600daea6e744dfd6dd35b
vanxh <hello@vanxh.dev>
2023-08-28 02:22:08 +0530

    mobile viewport

--- src/app/shared-metadata.ts
+++ src/app/shared-metadata.ts
@@ -5,6 +5,7 @@ export const defaultMetadata = {
   title: TITLE,
   description: DESCRIPTION,
   metadataBase: new URL("https://www.openbio.app"),
+  viewport: "width=device-width, initial-scale=1",
 };
 
 export const twitterMetadata = {
