6acf2f4689d8d2611337830eaab8eef43fdce041
vanxh <hello@vanxh.dev>
2023-08-31 14:58:13 +0530

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,15 @@ import type { MetadataRoute } from "next";
 const addPathToBaseURL = (path: string) => `https://www.openbio.app${path}`;
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ["/"].map((route) => ({
+  const routes = [
+    "/",
+    "/legal/privacy",
+    "/legal/terms",
+    "/create-link",
+    "/claim-link",
+    "/sign-in",
+    "/sign-up",
+  ].map((route) => ({
     url: addPathToBaseURL(route),
     lastModified: new Date(),
   }));
