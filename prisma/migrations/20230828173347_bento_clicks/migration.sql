d26913895d4eab52deeb6097b7440712535c830c
vanxh <hello@vanxh.dev>
2023-09-05 00:35:54 +0530

    use popover for hex color picker

--- prisma/migrations/20230828173347_bento_clicks/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Bento" ADD COLUMN     "clicks" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20230829223110_y/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "ProfileLinkView_ip_key";
-
--- CreateIndex
-CREATE INDEX "ProfileLinkView_ip_idx" ON "ProfileLinkView"("ip");
