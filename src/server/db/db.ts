62baaf191ccf039ee9ad4d4daf956e03ab1aff9f
vanxh <hello@vanxh.dev>
2023-09-15 02:04:47 +0530

    fix

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,16 +1,11 @@
 import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js";
 import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
-import postgres from "postgres";
 
 import { env } from "@/env.mjs";
 import * as schema from "./schema";
 
 neonConfig.fetchConnectionCache = true;
 
-export const db =
-  env.NODE_ENV === "production"
-    ? drizzleNeon(neon(env.DATABASE_URL), {
-        schema,
-      })
-    : drizzlePostgres(postgres(env.DATABASE_URL), { schema });
+export const db = drizzleNeon(neon(env.DATABASE_URL), {
+  schema,
+});
